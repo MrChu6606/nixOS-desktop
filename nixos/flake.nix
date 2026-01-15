a59e35375dd35f174e2b1e79bb28f309c89b0b80
MrChu6606 <nmcicchi@gmail.com>
2026-01-14 19:54:27 -0500

    rewrote system flake

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,29 +2,19 @@
   description = "My NixOS system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixvim.url = "path:../nixvim";
     librepods.url = "path:../librepods";
   };
 
-  outputs = { self, nixpkgs, nixvim, librepods }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-  in {
-    nixosConfigurations.lotus = pkgs.lib.nixosSystem {
-      inherit system;
-
-      modules = [
-        ./configuration.nix
-        nixvim.nixosModule
-      ];
-
-      configuration = {
-        environment.systemPackages = [
-          librepods.packages.${system}.librepods
-        ];
+  outputs = { self, nixpkgs, nixvim, librepods }: {
+    nixosConfigurations = {
+      lotus = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+	modules = [
+	 ./configuration.nix
+	];
       };
     };
   };
 }
-
