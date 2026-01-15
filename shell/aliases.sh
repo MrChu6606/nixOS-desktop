acacc903809908b570e4a784861b3a79f5d991bb
MrChu6606 <nmcicchi@gmail.com>
2026-01-15 17:29:13 -0500

    fixed update alias to commmit flake.lock and keep clean trees

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -7,6 +7,16 @@ nupdate() {
   flake_dir=${1:-$PWD}   # default to current dir if no argument
   echo "Updating flake in $flake_dir..."
   nix flake update "$flake_dir"
+
+  # Check if flake.lock changed
+  if ! git diff --quiet flake.lock; then
+    echo "Commiting update flake.lock..."
+    git add flake.lock
+    git commit -m "Update flake inputs"
+  else
+    echo "flake.lock unchanged, nothing to commit."
+  fi
+
   echo "Rebuilding system..."
   sudo nixos-rebuild switch --flake "$flake_dir#lotus"
 }
