e67e58f0a1daebf3e029c2a9793cd8d4cc7f36b4
MrChu6606 <nmcicchi@gmail.com>
2026-01-18 13:15:41 -0500

    Added virtmanager and enabled virtualization in config.nix

---
+++ modules/config.nix
@@ -0,0 +1,18 @@
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  # Enable virt-manager and virtualisation
+  virtualisation.libvirtd.enable = true;
+  virtualisation.spiceUSBRedirection.enable = true;
+
+  # Enable UEFI for modern OSes like Windows 11
+  virtualisation.libvirtd.qemu.ovmf.enable = true;
+
+  # Enable nested VMs
+  boot.extraModprobeConfig = "options kvm_intel nested=1";
+
+  programs.virt-manager.enable = true;
+  users.users.nix.extraGroups = ["libvirtd" "kvm"];
+}
