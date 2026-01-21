6aa73157dc2837fd82a4e1a4c60fe2ccc4dd5eaa
MrChu6606 <nmcicchi@gmail.com>
2026-01-20 23:06:32 -0500

    fixed typos in config.sh

--- nnn/config.sh
+++ nnn/config.sh
@@ -1,3 +1,3 @@
 #nnn configs
-export NNN_PLUG='c:cdpath;k:chksum;f:finder;z:fzcd;e:fzopen;g:getplugs;v:gpgv;i:imgview;p:ipinfo;s:mp3conv;t:preview-tui;r:rsynccp;d:suedit;x:togglex;l:x2sel;a:xdgdefault;'
+export NNN_PLUG='c:plugins/cdpath;k:plugins/chksum;f:plugins/finder;z:plugins/fzcd;e:plugins/fzopen;g:plugins/getplugs;v:plugins/gpgv;i:plugins/imgview;p:plugins/ipinfo;s:plugins/mp3conv;t:plugins/preview-tui;r:plugins/rsynccp;d:plugins/suedit;x:plugins/togglex;l:plugins/x2sel;a:plugins/xdgdefault;'
 export NNN_OPTS='-c -k -f -z -e -g -v -i -p -s -t -r -d -x -l -a'
