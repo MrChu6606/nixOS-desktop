0f9a7a9995cfed3468573075eb725ff8f6cbbdb1
MrChu6606 <nmcicchi@gmail.com>
2026-01-21 22:29:54 -0500

    Fixed cursor theme, and maybe something else?

--- nnn/config.sh
+++ nnn/config.sh
@@ -1,4 +1,4 @@
 #nnn configs
 export NNN_PLUGDIR="$HOME/dotfiles/nnn/plugins"
-export NNN_OPTS='-d -p'
+export NNN_OPTS=""
 export NNN_PLUG='c:cdpath;k:chksum;f:finder;z:fzcd;e:fzopen;g:getplugs;v:gpgv;i:imgview;p:ipinfo;s:mp3conv;t:preview-tui;r:rsynccp;d:suedit;x:togglex;l:x2sel;a:xdgdefault;'
