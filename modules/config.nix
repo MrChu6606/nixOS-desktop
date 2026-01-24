ee7bc8bbb8fc7ca5faa194b02fd3fd0beac43fa6
MrChu6606 <nmcicchi@gmail.com>
2026-01-24 12:13:37 -0500

    more tweaks to kernel to try and stop random touchpad shutdowns

--- modules/config.nix
+++ modules/config.nix
@@ -6,12 +6,24 @@
   # Set system kernel
   boot.kernelPackages = pkgs.linuxPackages;
 
-  # Makes shutdown and boot visible for debugging
+  # Configure system to not turn off touchpad
   boot.kernelParams = [
     "i915.enable_psr=0"
     "acpi_osi="
     "pcie_aspm=off"
+    "intel_idle.max_cstate=2"
   ];
+
+  boot.kernelModules = [
+    "i2c_hid"
+    "i2c_hid_acpi"
+    "hid_multitouch"
+  ]
+
+  services.udev.extraRules = ''
+      ACTION=="add", SUBSYSTEM=="usb", TEST=="power/control", ATTR{power/control}="on"
+      ACTION=="add", SUBSYSTEM=="i2c", TEST=="power/contol", ATTR{power/control}="on"
+    '';
 
   # Enable virt-manager and virtualisation
   #virtualisation.libvirtd.enable = true;
