ce0628601473b18bdde67e483a07f8dbd35f1338
Nicolas Cicchi <nmcicchi@gmail.com>
2026-02-09 23:28:38 -0500

    themed gdm

--- modules/greeter.nix
+++ modules/greeter.nix
@@ -11,5 +11,41 @@
       enable = true;
       wayland = true;
     };
+
+    displayManager.autoLogin = {
+      enable = true;
+      user = "nic";
+    };
   };
+
+  # set wallpaper
+  environment.etc."gdm-wallpaper.jpg".source = ../Wallpapers/red-forest.jpg;
+
+  programs.dconf.profiles.gdm.databases = [
+    {
+      settings = {
+        "org/gnome/desktop/interface" = {
+          color-scheme = "prefer-dark";
+          cursor-theme = "Numix-Cursor";
+          icon-theme = "Papirus-Dark";
+          font-name = "Cantarell 11";
+        };
+
+        "org/gnome/desktop/background" = {
+          picture-uri = "files://etc/gdm-wallpaper.jpg";
+          picture-uri-dark = "files://etc/gdm-wallpaper.jpg";
+        };
+
+        "org/gnome/login-screen" = {
+          disable-user-list = true;
+        };
+      };
+    }
+  ];
+
+  environment.systemPackages = with pkgs; [
+    papirus-icon-theme
+    gnome.adwaita-icon-theme
+    gnome-themes-extra
+  ];
 }
