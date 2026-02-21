4547a35d5960b6650e3ecc4b91a2d183661a2b2c
Nicolas Cicchi <nmcicchi@gmail.com>
2026-02-21 14:19:11 -0500

    hopefully fixed it fr this time

--- modules/greeter.nix
+++ modules/greeter.nix
@@ -1,20 +1,23 @@
-{
-  pkgs,
-  silentSDDM,
-  ...
-}: let
-  SilentTheme = silentSDDM;
+{ pkgs, silentSDDM, ... }:
 
-in {
+{
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
-      
-    theme = SilentTheme.pname;
-    extraPackages = [SilentTheme] ++ SilentTheme.propagatedBuildInputs ++ [ pkgs.qt6.qtmultimedia ];
+
+    # Theme name
+    theme = silentSDDM.pname;
+
+    # Inject the theme + Qt multimedia for SDDM
+    extraPackages = [
+      silentSDDM
+      pkgs.qt6.qtbase
+      pkgs.qt6.qtmultimedia
+      pkgs.qt6.qtwayland
+    ];
   };
 
-    environment.systemPackages = with pkgs; [
+  environment.systemPackages = with pkgs; [
     silentSDDM
     silentSDDM.test
   ];
