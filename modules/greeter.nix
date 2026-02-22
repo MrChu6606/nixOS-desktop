d3a8679ed28449e0f7939a77080fd7461cbde57d
Nicolas Cicchi <nmcicchi@gmail.com>
2026-02-21 19:42:02 -0500

    tweaking aspect ratio bug

--- modules/greeter.nix
+++ modules/greeter.nix
@@ -1,6 +1,12 @@
 { inputs, ... }:
 {
     imports = [inputs.silentSDDM.nixosModules.default];
+
+    services.displayManager.sddm = {
+        enable = true;
+        wayland.enable = true;
+    };
+
     programs.silentSDDM = {
         enable = true;
         theme = "default";
