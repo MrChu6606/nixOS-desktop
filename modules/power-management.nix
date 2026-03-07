3f0956e0b6f0f6c12d10fa6f17f17a57d2bee657
Nicolas Cicchi <nmcicchi@gmail.com>
2026-03-07 17:10:11 -0500

    removed bluray fix attempt cause it failed

--- modules/power-management.nix
+++ modules/power-management.nix
@@ -14,7 +14,4 @@
   services.power-profiles-daemon.enable = true;
   services.upower.enable = true;
   services.thermald.enable = true;
-
-  # attempt to make bluray drive work
-  boot.kernelParams = [ "usbcore.autosuspend=-1" ];
 }
