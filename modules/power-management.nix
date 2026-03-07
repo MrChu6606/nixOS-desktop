5ed8d98a6ce98ddf25825ff448044345c72073f0
Nicolas Cicchi <nmcicchi@gmail.com>
2026-03-07 16:53:31 -0500

    attempting to make bluray drive work

--- modules/power-management.nix
+++ modules/power-management.nix
@@ -14,4 +14,7 @@
   services.power-profiles-daemon.enable = true;
   services.upower.enable = true;
   services.thermald.enable = true;
+
+  # attempt to make bluray drive work
+  boot.kernelParams = [ "usbcore.autosuspend=-1" ];
 }
