f6b69c8fa20e854fb1b7400dd50c26366706f21d
MrChu6606 <>
2026-05-18 19:46:50 -0400

    it finally works

--- modules/server/system/mounts.nix
+++ modules/server/system/mounts.nix
@@ -1,12 +1,12 @@
 _: {
   fileSystems."/appdata" = {
-    device = "/dev/disk/by-partlabel/appdata";
+    device = "/dev/disk/by-uuid/aea08e72-e626-4162-9781-a47bec7f365b";
     fsType = "ext4";
     options = [ "noatime" ];
   };
 
   fileSystems."/media" = {
-    device = "/dev/disk/by-partlabel/media";
+    device = "/dev/disk/by-uuid/f9616b58-91c3-450b-8002-b9d9a0f55b5f";
     fsType = "ext4";
     options = [ "noatime" ];
   };
